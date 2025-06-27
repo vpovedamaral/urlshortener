a410c8f6fa22d79dc50e7b5e110516a4d82ed296
21Hmzz <hamza.bella2107@gmail.com>
2025-06-27 16:10:45 +0200

    refactor:commentaires

--- internal/models/link.go
+++ internal/models/link.go
@@ -6,14 +6,7 @@ import (
 	"gorm.io/gorm"
 )
 
-// TODO : Créer la struct Link
 // Link représente un lien raccourci dans la base de données.
-// Les tags `gorm:"..."` définissent comment GORM doit mapper cette structure à une table SQL.
-// ID qui est une primaryKey
-// Shortcode : doit être unique, indexé pour des recherches rapide (voir doc), taille max 10 caractères
-// LongURL : doit pas être null
-// CreateAt : Horodatage de la créatino du lien
-
 type Link struct {
 	ID        uint           `gorm:"primaryKey" json:"id"`
 	ShortCode string         `gorm:"unique;not null;size:6;index" json:"short_code"`
@@ -23,7 +16,7 @@ type Link struct {
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 
-	// Relation avec les clics - un lien peut avoir plusieurs clics
+	// Relation un-à-plusieurs avec les clics.
 	Clicks []Click `gorm:"foreignKey:LinkID" json:"clicks,omitempty"`
 }
 
