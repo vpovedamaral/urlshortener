0af6719ddb27f2f15bbed359805f27994ab98d2d
21Hmzz <hamza.bella2107@gmail.com>
2025-06-27 16:10:56 +0200

    refactor:commentaires

--- internal/services/click_service.go
+++ internal/services/click_service.go
@@ -2,35 +2,27 @@ package services
 
 import (
 	"github.com/axellelanca/urlshortener/internal/models"
-	"github.com/axellelanca/urlshortener/internal/repository" // Importe le package repository
+	"github.com/axellelanca/urlshortener/internal/repository"
 )
 
-// TODO : créer la struct
-// ClickService est une structure qui fournit des méthodes pour la logique métier des clics.
-// Elle est juste composer de clickRepo qui est de type ClickRepository
+// ClickService fournit la logique métier pour les clics.
 type ClickService struct {
 	clickRepo repository.ClickRepository
 }
 
-// NewClickService crée et retourne une nouvelle instance de ClickService.
-// C'est la fonction recommandée pour obtenir un service, assurant que toutes ses dépendances sont injectées.
+// NewClickService crée une nouvelle instance de ClickService.
 func NewClickService(clickRepo repository.ClickRepository) *ClickService {
 	return &ClickService{
 		clickRepo: clickRepo,
 	}
 }
 
-// RecordClick enregistre un nouvel événement de clic dans la base de données.
-// Cette méthode est appelée par le worker asynchrone.
+// RecordClick enregistre un nouvel événement de clic.
 func (s *ClickService) RecordClick(click *models.Click) error {
-	// TODO 1: Appeler le ClickRepository (CreateClick) pour créer l'enregistrement de clic.
-	// Gérer toute erreur provenant du repository.
 	return s.clickRepo.CreateClick(click)
 }
 
-// GetClicksCountByLinkID récupère le nombre total de clics pour un LinkID donné.
-// Cette méthode pourrait être utilisée par le LinkService pour les statistiques, ou directement par l'API stats.
+// GetClicksCountByLinkID récupère le nombre total de clics pour un lien donné.
 func (s *ClickService) GetClicksCountByLinkID(linkID uint) (int, error) {
-	// TODO 2: Appeler le ClickRepository (CountclicksByLinkID) pour compter les clics par LinkID.
 	return s.clickRepo.CountClicksByLinkID(linkID)
 }
